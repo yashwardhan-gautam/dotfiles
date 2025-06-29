08b7031f9775d781d22eba205717bb10e37e477c
yashwardhna-gautam <gautamyashwardhan@gmail.com>
2025-06-29 05:51:04 +0530

    Update Nix configuration: add scripts import, update home.stateVersion to 25.11, and clean up package list formatting for improved readability.

--- home/default.nix
+++ home/default.nix
@@ -1,21 +1,18 @@
+{ config, lib, pkgs, inputs, ... }:
+
 {
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}: {
   imports = [
     ./programs/default.nix
     ./utilities/default.nix
     ./themes/default.nix
+    ./scripts/default.nix
   ];
 
   # Home Manager basic settings
   home.username = "unalome";
   home.homeDirectory = "/home/unalome";
-  home.stateVersion = "25.05";
-
+  home.stateVersion = "25.11";
+  
   # Basic packages
   home.packages = with pkgs; [
     # Development tools
@@ -23,7 +20,7 @@
     python3
     rustc
     cargo
-
+    
     # System utilities
     tree
     eza
@@ -31,15 +28,15 @@
     fd
     ripgrep
     fzf
-
+    
     # Media
     mpv
     ffmpeg
-
+    
     # Other
     qalculate-gtk
     gimp
   ];
 
   programs.home-manager.enable = true;
-}
+} 
